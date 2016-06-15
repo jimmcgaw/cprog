4e553d452c0a679aaf8648b3b53e5ee915ed7591
jimmcgaw <jpmcgaw@gmail.com>
2016-06-14 17:19:38 -0700

    fibonacci function

--- hello.c
+++ hello.c
@@ -12,6 +12,15 @@ void stateHeight(int heightInInches){
   printf("I am %d inches tall.\n", heightInInches);
 }
 
+double cfibonacci(int n){
+  int i;
+  double a = 0.0, b = 1.0, tmp;
+  for (i = 0; i < n; ++i){
+    tmp = a; a = a + b; b = tmp;
+  }
+  return a;
+}
+
 int main(int argc, char *argv[]){
 
   char *greeting = argv[1];
@@ -22,6 +31,12 @@ int main(int argc, char *argv[]){
 
   stateAge(age);
   stateHeight(height);
+
+  int areas[] = {10, 12, 13, 14, 20};
+  printf("The size of an int is %1lu\n", sizeof(int));
+  printf("The length of the areas array is %1lu\n", sizeof(areas) / sizeof(int));
+  
+  printf("10th number of the Fibonacci sequence is: %f", cfibonacci(10));
 
   return 0;
 }
